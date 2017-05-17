43818e73e20df1a3584dd875387a6b0e700228eb
lacau <llacau@gmail.com>
2017-05-17 00:33:35 -0300

    Define pins and initialization.

--- vibration-detect/src/main.cpp
+++ vibration-detect/src/main.cpp
@@ -1,7 +1,16 @@
 #include <Arduino.h>
 
+#define VIB_SENSOR_PIN 0
+#define RED_LED_PIN 7
+#define GREEN_LED_PIN 8
+
 void setup() {
-  }
+  pinMode(VIB_SENSOR_PIN, INPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  Serial.begin(9600);
+}
 
 void loop() {
+  
 }
